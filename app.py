7a56fad00f7dfe6cceefcaec8837deefd4567246
Falaye Promise <promiseanuoluwa@gmail.com>
2022-10-26 22:05:38 +0100

    Created database models for user and article

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from datetime import datetime
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///'+ os.path.join(base_dir,'blog.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+app.config['SECRET_KEY'] = 'b10c5e66032f14ac9208be5d'
 
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    username = db.Column(db.String(50), unique=True)
+    password_hash = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"User <{self.username}>"
+
+class Article(db.Model):
+    id =  db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    content = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.Datetime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f"Article <{self.title}>"
 
 # home page
 @app.route('/')
