edd65b3eb7df589e8b7b5998f1beb49e03ef4f24
Falaye Promise <promiseanuoluwa@gmail.com>
2022-10-26 21:36:54 +0100

    Created the routes

--- app.py
+++ app.py
@@ -10,3 +10,48 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 db = SQLAlchemy(app)
+
+# home page
+@app.route('/')
+def home():
+    return 'home'
+
+# about page
+@app.route('/about')
+def about():
+    return 'about'
+
+# contact page
+@app.route('/contact')
+def contact():
+    return 'contact'
+
+# login page
+@app.route('/login')
+def login():
+    return 'login'
+
+# register page
+@app.route('/register')
+def signup():
+    return 'signup'
+
+# logout page
+@app.route('/logout')
+def logout():
+    return 'logout'
+
+# Individual blog post - /<name_of_blog>
+@app.route('/<name_of_blog>')
+def single_blog(name_of_blog):
+    return 'blog'
+
+# create
+@app.route('/create')
+def create():
+    return 'create'
+
+# /<name_of_blog>/edit
+@app.route('/<name_of_blog>/edit')
+def edit(name_of_blog):
+    return 'edit'
